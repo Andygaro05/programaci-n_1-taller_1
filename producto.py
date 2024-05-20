601ce6857ddf929bfbdd20b9d5535b7cf53b8cd9
SSamuelP <samuelsantiagossp10@gmail.com>
2024-05-20 02:26:30 -0500

    Correcciones de presentación

--- producto.py
+++ producto.py
@@ -27,3 +27,10 @@ productos = [Producto("Silla Reclinable", "Espectacular silla amoblada para una 
                 Producto("Silla Computador", "Silla para escritorio hecha en tela y con soporte hasta de 80kg", 125000, categorias[4]),
                 Producto("Biblioteca Horizontal", "Biblioteca horizontal multiusos color madera oscura, cuenta con seis espacios para almacenar", 560000, categorias[4]),
                 Producto("Set de Herramientas Manuales de 250 Piezas", "Juego completo de herramientas que cuenta con 250 piezas", 209900, categorias[2])]
+
+#Definimos qué productos pertenecerán a las categorias
+categorias[2].agregar_producto(productos[4])
+categorias[4].agregar_producto(productos[0])
+categorias[4].agregar_producto(productos[1])
+categorias[4].agregar_producto(productos[2])
+categorias[5].agregar_producto(productos[3])
